da02aacce254a18a3a0dceb59370f326c6b30b05
Brandon Pitts <Brandomoki.bp@gmail.com>
2022-10-20 18:18:39 +0000

    added logic.js, helper function to fetchJobsApi

---
+++ logic.js
@@ -0,0 +1,34 @@
+const axios = require("axios");
+// const { addS3Object, getS3Object, deleteS3Object } = require('./s3');
+
+const options = {
+  method: 'GET',
+  url: 'https://jsearch.p.rapidapi.com/search',
+  params: {
+    query: 'software developer',
+    page: '1',
+    date_posted: 'today',
+    remote_jobs_only: 'true',
+    num_pages: '1'
+  },
+  headers: {
+    'X-RapidAPI-Key': 'd0e5059415msh584763f07ba2650p1f773ejsnca44711c75f5',
+    'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+  }
+};
+
+module.exports.fetchJobsApi = async function fetchJobsApi() {
+    let config = {
+        timeout: 6500
+    }
+
+    try{
+    const response = await axios.request(options, config);
+	
+    return response.data;
+    } catch (error) {
+        console.log('^^^^^^^^^^ERROR', error);
+        return null;
+    }
+}
+
